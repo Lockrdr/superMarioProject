17f499da1923ae5069c671cedab90b9174dc0112
Ricardo de la Rosa <ricardo@mitmiapp.com>
2015-04-19 12:47:06 +0200

    Component deafaultEnemy and animations added

--- game.js
+++ game.js
@@ -8,7 +8,28 @@ var Q = Quintus().include("Sprites,TMX, Scenes, Input, 2D, Anim, Touch, UI")
 			height: 480})
 	.controls().touch();
 
+Q.animations("marioAnim", {
 
+	run_right: {frames:[1,2,3],rate: 1/10},
+	run_left: {frames: [15,16,17],rate: 1/10},
+	stand_right: {frames:[0],rate:1/15},
+	stand_left: {frames:[14],rate:1/15},
+	jump_right:{ frames: [4], rate: 1/50, loop: false},
+	jump_left:{ frames: [18], rate: 1/50, loop: false}
+
+
+});
+
+Q.animations("bloopaAnim", { 
+
+		move: {frames: [0,1], rate: 1/2},
+		die: {frames: [2], rate: 1/2, loop:false, trigger: "deathEvent"}
+});
+
+Q.animations("goombaAnim",{ 
+	move: {frames: [0,1], rate: 1/5},
+	die: {frames: [2], rate: 1/5, loop:false, trigger: "deathEvent"}
+});
 
 Q.Sprite.extend("Mario",{
 
@@ -17,12 +38,14 @@ Q.Sprite.extend("Mario",{
 
 	this._super(p, {
 		sheet: "marioR",
+		sprite:"marioAnim",
 		x: 50,
-		y: 500
+		y: 500,
+		jumpSpeed:-600
 
 	});
 
-	this.add("2d,platformerControls");
+	this.add("2d,platformerControls,animation");
 	},
 
 	step: function(dt){
@@ -32,6 +55,14 @@ Q.Sprite.extend("Mario",{
 			 Q.stageScene("endGame",1, { label: "You died" });
 			 this.destroy();
 		}
+		
+		if(this.p.vx==0)
+			this.play("stand_"+this.p.direction);		
+		else
+			this.play("run_"+this.p.direction);
+
+		if(this.p.vy!=0)
+			this.play("jump_"+this.p.direction);
 
 	}
 
@@ -42,30 +73,14 @@ Q.Sprite.extend("Goomba",{
 	init:function(p){
 		this._super(p,{
 			sheet:"goomba",
+			sprite:"goombaAnim",
 			x:300,
 			y:500,
 			vx:-80
 
 		});
-	this.add("2d,aiBounce");
+	this.add("2d,aiBounce,animation,defaultEnemy");
 
-	this.on("bump.left,bump.right",function(collision){
-
-		if(collision.obj.isA("Mario"))
-		{
-			
-			Q.stageScene("endGame",1, { label: "You died" });
-				collision.obj.destroy();
-		}
-	});
-
-	this.on("bump.top",function(collision){
-		if(collision.obj.isA("Mario")){
-			this.destroy();
-			collision.obj.p.vy = -300;
-
-		}
-	});
 
 	}
 
@@ -78,32 +93,20 @@ Q.Sprite.extend("Bloopa",{
 	init:function(p){
 		this._super(p,{
 			sheet:"bloopa",
+			sprite:"bloopaAnim",
 			x:170,
 			gravity:0.2,
 			y:400,
 			initY:400,
 			downardRange:100,
-			upwardSpeed:178
+			upwardSpeed:202
 
 			
 
 		});
-	this.add("2d");
-	this.on("bump.bottom,bump.left,bump.right",function(collision){
+	this.add("2d,animation,defaultEnemy");
 
-		if(collision.obj.isA("Mario"))
-		{
-				Q.stageScene("endGame",1, { label: "You died" });
-				collision.obj.destroy();
-		}
-	});
 
-	this.on("bump.top",function(collision){
-		if(collision.obj.isA("Mario")){
-			this.destroy();
-			collision.obj.p.vy = -300;
-		}
-	});
 
 	},
 	step: function(dt){
@@ -111,14 +114,41 @@ Q.Sprite.extend("Bloopa",{
 		if(this.p.y>=this.p.initY+this.p.downardRange){
 			this.p.vy= -this.p.upwardSpeed;
 		}
+		
 	}
 });
+
+Q.component("defaultEnemy",{
+
+	added: function(){
+
+		this.entity.play("move");
+
+	this.entity.on("bump.bottom,bump.left,bump.right",function(collision){
+		if(collision.obj.isA("Mario"))
+		{
+				Q.stageScene("endGame",1, { label: "You died" });
+				collision.obj.destroy();
+		}
+	 });
+
+	this.entity.on("bump.top",function(collision){
+		if(collision.obj.isA("Mario")){
+			this.play("die");
+			collision.obj.p.vy = -300;
+		}
+	});
+
+	this.entity.on("deathEvent",this,function(){this.entity.destroy()});
+	},
+
+})
 
 Q.Sprite.extend("Princess",{
 	init:function(p){
 		this._super(p,{
 			sheet:"princess",
-			x:90,
+			x:1000,
 			y:400
 			
 			
